75dcc24c9ed858585575f25f456b2478ea2133ef
Eric Rieman <er28@zips.uakron.edu>
2021-05-30 23:12:05 -0400

    Init server

--- sandbox-react/src/index.js
+++ sandbox-react/src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const express = require("express")
+const cors = require("cors")
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json()) //gives us req.body
+
+//routes
+
+app.listen( 5000, () => {
+  console.log("server is starting on part 5000")
+})
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
