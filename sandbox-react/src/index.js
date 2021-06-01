01d54175653abce662d3aaaf52720be08924ea14
Eric Rieman <er28@zips.uakron.edu>
2021-05-31 21:05:16 -0400

    Created the database

--- sandbox-react/src/index.js
+++ sandbox-react/src/index.js
@@ -4,13 +4,22 @@
 
 const express = require("express")
 const cors = require("cors")
-const app = express();
+const app = express()
+const pool = require("./dp")
 
 //middleware
 app.use(cors())
 app.use(express.json()) //gives us req.body
 
 //routes
+
+app.get("/users", async(req,res) => {
+  try {
+    res.json(req.query)
+  } catch(err) {
+    console.error(err.message);
+  }
+})
 
 app.listen( 5000, () => {
   console.log("server is starting on part 5000")
