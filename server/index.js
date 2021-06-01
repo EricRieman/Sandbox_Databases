3a009c6015062a83095d9179eb711e10464b9cb5
Eric Rieman <er28@zips.uakron.edu>
2021-05-31 23:50:50 -0400

    Removed unused items from workspace

--- server/index.js
+++ server/index.js
@@ -5,24 +5,31 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
-const pool = require("./dp")
+const pool = require("./db")
 
 //middleware
 app.use(cors())
-app.use(express.json()) //gives us req.body
+//app.use(express.json()) //gives us req.body
 
 //routes
 
+//query parameters: allow us to send data in our url: http://localhost:5000/?name=henry = req.query
+//params: allows us to identify a specific resource: http://localhost:5000/:id = req.params
+
 app.get("/users", async(req,res) => {
   try {
-    res.json(req.query)
+    const {name} = req.query
+    const users = await pool.query( "SELECT * FROM users")
+
+    res.json(users.rows)
+
   } catch(err) {
     console.error(err.message);
   }
 })
 
 app.listen( 5000, () => {
-  console.log("server is starting on part 5000")
+  console.log("server is starting on port 5000")
 })
 
 // ReactDOM.render(
