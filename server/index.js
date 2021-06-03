90faf74b5614c44646416a7e296ddb6343f1dbd0
Eric Rieman <er28@zips.uakron.edu>
2021-06-03 00:31:18 -0400

    Preparing client side

--- server/index.js
+++ server/index.js
@@ -11,11 +11,21 @@ app.use(cors())
 
 //query parameters: allow us to send data in our url: http://localhost:5000/?name=henry = req.query
 //params: allows us to identify a specific resource: http://localhost:5000/:id = req.params
-// we can testthe below request using this link in postman: http://localhost:5000/users/?name=Dun
+// we can test the below request using this link in postman: http://localhost:5000/users/?inputName=he
 app.get("/users", async(req,res) => {
   try {
-    const {name} = req.query
-    const users = await pool.query( "SELECT * FROM users WHERE first_name = '" + name + "'")
+    const {inputName} = req.query //it is important the name here matches the name in the query parameters
+    //const users = await pool.query( "SELECT * FROM users WHERE first_name = '" + name + "'")
+
+    // For a more advanced query that will return any first or last name form the matching charaters,
+    // lets disect the querry below using the LIKE operator and %{}%
+
+    //first, we concat the first and last name using ||
+    const firstAndLast = "first_name || ' ' || last_name";
+
+    // next we use the LIKE operator and define a pattern
+    // but because we are using postgres, we can instead use ILIKE, which makes the search case-insensitive
+    const users = await pool.query( "SELECT * FROM users WHERE " + firstAndLast + " ILIKE $1", [`%${inputName}%`]) //make sure to use `` here!
 
     res.json(users.rows)
 
