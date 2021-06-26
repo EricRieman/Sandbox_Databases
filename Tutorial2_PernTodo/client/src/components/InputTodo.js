5f5d182efa1f8cae905b75fc136a71881db6bf48
Eric Rieman <er28@zips.uakron.edu>
2021-06-26 02:16:14 -0400

    Added placeholder to text input on pern todo

--- Tutorial2_PernTodo/client/src/components/InputTodo.js
+++ Tutorial2_PernTodo/client/src/components/InputTodo.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
 const InputTodo = () => {
-  const [description, setDescription] = useState("hello")
+  const [description, setDescription] = useState("")
   
   const onSubmitForm = async e => {
     e.preventDefault()
     try {
       const body = {description}
+
       const resp = await fetch("http://localhost:5000/sandbox",{
         method: "POST",
         headers: {"Content-Type": "application/json"},
@@ -24,7 +25,7 @@ const InputTodo = () => {
     <>
       <h1 className="text-center mt-5">Pern ToDo List</h1>
       <form className="d-flex mt-5" onSubmit={onSubmitForm} >
-        <input type ="text" className="form-control" value={description} onChange={(e)=>setDescription(e.target.value)}/>
+        <input type ="text" className="form-control" placeholder="I need to..." value={description} onChange={(e)=>setDescription(e.target.value)}/>
         <button className = "btn btn-success ml-2">Add</button>
       </form>
     </>
