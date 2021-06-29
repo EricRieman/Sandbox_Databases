6c80651fb2798a77ed8b2221b717afeae818a7e3
Eric Rieman <er28@zips.uakron.edu>
2021-06-29 00:31:21 -0400

    Added missing file for NodeExpress module tutorial

--- Tutorial3_NodeExpress/1-Modules/MindGenade/App.js
+++ Tutorial3_NodeExpress/1-Modules/MindGenade/App.js
@@ -1,1 +1,5 @@
+//if there is a function inside of the module that gets invoked, that function
+//will run, even if we dont assign it to a variable or export anything from the 
+//module. The key being whenever you require a module, you invoke it
+
 require('./MindGenade')
